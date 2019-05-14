9d00d4ebf256256a29aceee72de83254406b4c7f
tff <tff123>
2019-05-14 09:20:53 +0800

    Initial commit

---
+++ JsoupHelperLib/src/main/java/com/thomas/jsoup/JsoupHelper.java
@@ -0,0 +1,15 @@
+package com.thomas.jsoup;
+
+import android.util.Log;
+
+/**
+ * @author Thomas
+ * @date 2019/5/13
+ * @updatelog
+ */
+public class JsoupHelper {
+
+    public static void sayHello(){
+        Log.e("jsouphelper","sayHello");
+    }
+}
